f632b6531829e26c187beeb7b719bd8d036c35ab
VIROJA KUNJ BHARATBHAI <23it138@charusat.edu.in>
2025-05-24 14:17:22 +0530

    Delete WeatherNow_B-main directory

--- WeatherNow_B-main/test-env.js
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({ path: path.resolve("./.env") });
-
-console.log("Test: OPENAI_API_KEY =", process.env.OPENWEATHER_API_KEY ? "FOUND" : "NOT FOUND");
